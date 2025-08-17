1a747a6180f762e419cde0834f540731c27b0994
jfventures97 <166265786+jfventures97@users.noreply.github.com>
2025-08-17 22:40:09 +0800

    Added featured post skeleton

---
+++ data.js
@@ -0,0 +1,14 @@
+[
+  {
+    "title": "My new journey as a bootcamp student",
+    "date": "July 23, 2022",
+    "text": "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
+    "image": "images\laptopimage.png"
+  },
+  {
+    "title": "Blog One",
+    "date": "July 23, 2022",
+    "text": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "image": "images\article-image-03.png"
+  }
+]
